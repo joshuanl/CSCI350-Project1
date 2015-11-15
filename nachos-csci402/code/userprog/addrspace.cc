1eaf31798617734774d028c0b254dbdbe495a4f6
yglazko <yglazko@MacBook-Pro-2.local>
2015-11-15 10:04:17 -0800

    stuff

--- nachos-csci402/code/userprog/addrspace.cc
+++ nachos-csci402/code/userprog/addrspace.cc
@@ -141,7 +141,6 @@ AddrSpace::AddrSpace(OpenFile *executable) : fileTable(MaxOpenFiles) {
 						// to run anything too big --
 						// at least until we have
 						// virtual memory
-    MutexLock l(&page_manager->lock_);
     if (page_manager->num_available_pages() < numPages) {
         printf("OOM. Terminating.\n");
         interrupt->Halt();
